d61af40b8862facb454880b9db7a9556571e131f
Vishnu Vijayan <vvijayan@purdue.edu>
2017-11-09 12:22:29 -0500

    Parse response data into a dictionary

--- cryptoprice.py
+++ cryptoprice.py
@@ -1,11 +1,16 @@
-import urllib2
+import urllib
 import sys
+import json
 
 args = sys.argv[1:]
-cryptoData = urllib2.urlopen('https://api.coinmarketcap.com/v1/ticker/').read()
+response = urllib.urlopen('https://api.coinmarketcap.com/v1/ticker/')
+cryptoData = json.loads(response.read().decode())
 crpytoDataMap = {} # Dictionary for faster access
 
+for data in cryptoData:
+    crpytoDataMap[data['symbol']] = data
 
 print args
+print crpytoDataMap['LTC']['price_usd']
 
 

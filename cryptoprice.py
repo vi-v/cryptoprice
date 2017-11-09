4ec3a6a8bad7a9643f6d17919e1ad04da9a56911
Vishnu Vijayan <vvijayan@purdue.edu>
2017-11-09 13:32:22 -0500

    Add -a parameter

--- cryptoprice.py
+++ cryptoprice.py
@@ -6,14 +6,25 @@ args = sys.argv[1:]
 response = urllib.urlopen('https://api.coinmarketcap.com/v1/ticker/')
 cryptoData = json.loads(response.read().decode())
 crpytoDataMap = {} # Dictionary for faster access
+findall = False
+allCoins = []
 
 # Terminal colors
 OKGREEN = '\033[92m'
 FAIL = '\033[91m'
 RESET = '\033[0;0m'
 
+if args[0] == '-a':
+    findall = True
+
 for data in cryptoData:
     crpytoDataMap[data['symbol']] = data
+
+    if findall:
+        allCoins.append(data['symbol'])
+
+if findall:
+    args = allCoins
 
 for arg in args:
     arg = arg.strip().replace('-', '')
@@ -26,5 +37,9 @@ for arg in args:
     else:
         changeColor = OKGREEN
 
-    print "%s%s (%s) \t| USD: %s \t| %% Change 24H: %s%s%s" % (RESET, coinName, arg, priceUSD, changeColor, percentChange24, RESET)
+    col1 = coinName + ' (' + arg + ')'
+    col2 = 'USD: ' + priceUSD
+    col3 = '% Change 24H: ' + changeColor + percentChange24
+
+    print "%s%-30s | %-20s| %s %s" % (RESET, col1, col2, col3, RESET)
     
